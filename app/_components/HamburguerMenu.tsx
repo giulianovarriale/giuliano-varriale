3947733894c7851ca9f222fe8dae516ac8a3f54d
Giuliano Varriale <giuliano.oca@gmail.com>
2025-01-05 20:40:02 +0100

    Setup header and footer

---
+++ app/_components/HamburguerMenu.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import { Menu, X } from 'lucide-react';
+import { type ReactNode, useState } from 'react';
+
+type Props = {
+  navigation: ReactNode;
+};
+
+export function HamburguerMenu({ navigation }: Props) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <>
+      <button
+        className="w-12 h-12 inline-flex justify-center items-center"
+        aria-label="Toggle navigation menu"
+        onClick={() => setIsOpen(true)}
+      >
+        <Menu size={32} />
+      </button>
+
+      <div
+        className={`${
+          isOpen ? 'flex flex-col' : 'hidden'
+        } absolute w-full h-full top-0 left-0 bg-white z-50 p-9`}
+      >
+        <button
+          className="w-12 h-12 inline-flex justify-center items-center self-end"
+          aria-label="Close navigation menu"
+          onClick={() => setIsOpen(false)}
+        >
+          <X />
+        </button>
+
+        {navigation}
+      </div>
+    </>
+  );
+}
